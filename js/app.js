0b8fd1b96ade979424ddeb264afac3cd40b4498a
Dani163d <Danielasilvacastillo163@gmail.com>
2024-10-03 11:02:12 -0400

    impriminedo el resto de la infomacion

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
 const monedaSelect = document.querySelector('#moneda');
 const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
 
 const objBusqueda = {
     moneda: '',
@@ -98,5 +99,37 @@ function submitFormulario(e) {
     }
 
     function mostarCotizacionHTML(cotizacion) {
-        console.log(cotizacion);
+
+        lipiarHTML()
+
+        const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = cotizacion;
+
+        const precio = document.createElement('p');
+        precio.classList.add('precio');
+        precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+        const precioAlto = document.createElement('p');
+        precioAlto.innerHTML = `<p>Precio mas alto del dia: <span>${HIGHDAY}</span>`
+
+        const precioBajo = document.createElement('p');
+        precioBajo.innerHTML = `<p>Precio mas alto del dia: <span>${LOWDAY}</span>`
+
+        const ultimasHoras = document.createElement('p');
+        ultimasHoras.innerHTML = `<p>Variacion ultimas 24 horas: <span>${CHANGEPCT24HOUR}%</span>`
+
+        const ultimaActualizacion = document.createElement('p');
+        ultimaActualizacion.innerHTML = `<p>Ultima actualizacion del dia: <span>${LASTUPDATE}</span>`
+
+
+        resultado.appendChild(precio);
+        resultado.appendChild(precioAlto);
+        resultado.appendChild(precioBajo);
+        resultado.appendChild(ultimasHoras);
+        resultado.appendChild(ultimaActualizacion);
+    }
+
+    function lipiarHTML() {
+        while(resultado.firstChild){
+            resultado.removeChild(resultado.firstChild);
+        }
     }
